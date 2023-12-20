695fe0cc17c536a1e2df8412623d0c3e6da4f405
Tonega <thonieetega182@gmail.com>
2023-12-20 12:04:15 +0100

    React

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -35,10 +35,7 @@ const ThankYou = ({ cardDetails }) => {
         <p id="text-end">We've added your card details</p>
       </div>
     </main>
-    <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://github.com/Tonega">Tony</a>.
-         </div>
+    
     </>
   );
 };
